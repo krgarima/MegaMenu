68f695868764f4613f856cf7cddd252c9555694c
krgarima <kumargarima59@gmail.com>
2024-03-08 23:56:56 +0530

    style: gap in nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import Menubar from "./Menubar";
 
 const Navbar = () => {
   return (
-    <nav className="w-full h-12 bg-black text-gray-400 flex gap-x-10 items-center md:text-lg font-semibold px-12">
+    <nav className="w-full h-12 bg-black text-gray-400 flex gap-x-6 md:gap-x-12 items-center md:text-lg font-semibold px-6 md:px-12">
       {/* Logo */}
       <div>Prime</div>
       <Menubar />
